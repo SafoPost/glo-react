fcbc2b8b644156991e6ebaec4fa842794d4f43c9
SafoPost <s.i.post@mail.ru>
2020-10-25 17:09:35 +0500

    fix script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-'use strict';
-
-const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
-var nameCourse = 'Базовый React';
-var command = [];
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений',
+	' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
+const nameCourse = 'Базовый React';
+const command = [];
 for (let i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].trim() != '') {
+	if (employers[i].length > 0 && employers[i].trim() !== '') {
 		command.push(employers[i]);
 	}
 }
@@ -13,7 +12,7 @@ for (let i = 0; i < command.length; i++) {
 	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
 }
 
-var data = {
+const data = {
 	cash: [3, 8, 3],
 	react: ['JSX', 'components', 'props', 'state', 'hooks'],
 	add: ['styled-components', 'firebase']
@@ -21,21 +20,21 @@ var data = {
 
 function calcCash(own) {
 	own = own || 0;
-	var everyCash = Array.prototype.slice.call(arguments);
-	var total = own;
-	for (var i = 0; i < everyCash[1].length; i++) {
+	const everyCash = Array.prototype.slice.call(data.cash);
+	let total = own;
+	for (let i = 0; i < everyCash[1].length; i++) {
 		total += +everyCash[1][i];
 	}
 	return total;
 }
 
-var lesson = calcCash(null, data.cash);
+const lesson = calcCash(null, data.cash);
 
 function makeBusiness(director, teacher, allModule, gang, course) {
 	teacher = teacher || 'Максим';
-	var sumTech = data.react.concat(data.add, 'и другие');
-	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director + ', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' +
-		gang);
+	const sumTech = data.react.concat(data.add, 'и другие');
+	console.log('Стартуем новый курс: "' + course + '". Владелец: ' + director +
+		', преподаватель: ' + teacher + '. Всего уроков: ' + allModule + '. \nКоманда Академии: ' + gang);
 	console.log('Первое что изучим будет ' + data.react[0] + ". Он очень похож на HTML!");
 	console.log('Технологии которые мы изучим: ');
 	console.log.apply(null, sumTech);
